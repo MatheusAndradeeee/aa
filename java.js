1dc421c01db8307b345732e798c717e8b83f5c8c
MatheusAndradeeee <115434474+MatheusAndradeeee@users.noreply.github.com>
2024-11-05 16:51:47 -0400

    sim

---
+++ java.js
@@ -0,0 +1,7 @@
+// script.js
+document.getElementById('iniciar').addEventListener('click', function() {
+    const audio = new Audio("./audio/aaa.mp3");
+    
+    // Reproduzir o áudio
+    audio.play();
+});
